6a4259ebbc882b74b4ae36a0c7761eb458526309
Sqatcher <kacper.krzywicki@wp.pl>
2023-06-22 17:15:55 +0200

    Added some missing labs

--- Lab1/Python/SparkAppPython/main.py
+++ Lab1/Python/SparkAppPython/main.py
@@ -1,12 +1,21 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import random
 
 if __name__ == '__main__':
     spark = SparkSession.builder.master("local[*]").appName("HelloWorld").getOrCreate()
 
-    kolumny = ["Spark Version", "Scala Version", "Date"]
+    # kolumny = ["Spark Version", "Scala Version", "Date"]
+    # dane = [("3.1.2", "2.12", "May, 2021"), ("3.1.1", "2.12", "Mar, 2021"), ("3.1.0", "2.12", "Jan, 2021")]
+    # dfFromData = spark.createDataFrame(dane).toDF(*kolumny)
+    # dfFromData.show()
 
-    dane = [("3.1.2", "2.12", "May, 2021"), ("3.1.1", "2.12", "Mar, 2021"), ("3.1.0", "2.12", "Jan, 2021")]
+    df = spark.read.format("csv").load("data.csv")
 
-    dfFromData2 = spark.createDataFrame(dane).toDF(*kolumny)
+    def addRandom(num):
+        return num + random.random() - 0.5
 
-    dfFromData2.show()
+
+    df.select(col("gamesWon"), addRandom(col("gamesWon"))).show()
+
+
